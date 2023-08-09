bc063b320d72ceab034c03d1a231a5cb9a6b753f
JobHaver <cajhbru@gmail.com>
2023-08-08 22:45:03 -0700

    added dynamic colors

--- js/thunderdome.js
+++ js/thunderdome.js
@@ -12,7 +12,8 @@ app.data = {
 	playlist: [],
 	token: 0,
 	chosen_songs: [],
-	cloak: false
+	cloak: false,
+	canvas: document.createElement('canvas')
 };
 
 //1138
@@ -78,15 +79,33 @@ get_tracks = () => {
 };
 
 play = (player, albumA, albumB) => {
-	document.getElementById(albumB).style.filter = 'grayscale(1)';
-	document.getElementById(albumA).style.transform = 'scale(1.03)';
+	//var canvas = document.createElement('canvas');
+	var canvas = app.data.canvas;
+	var context = canvas.getContext('2d');
+	var img = document.getElementById(albumA);
+	img.crossOrigin = "Anonymous";
+	canvas.width = img.width; // these two needed so it doesnt glitch out
+	canvas.height = img.height;
+	context.drawImage(img, 0, 0);
+	var p = context.getImageData(getRandomInt(img.width), getRandomInt(img.height), 1, 1).data;
+	var hex = '#' + p[0].toString(16) + p[1].toString(16) + p[2].toString(16);
+	// console.log("x: " + x + " y: " + y + " hex: " + hex + " p: " + p);
+	//console.log(" hex: " + hex + " p: " + p);
+	//DocumentElement is HTML
+	if(p[3]){ //need to do this bc p does not work the first time, also error handeling
+		document.documentElement.style.backgroundColor = hex;
+	}
+	
 	document.getElementById(player).play();
+	// document.getElementById(albumB).style.filter = 'grayscale(1)';
+	// document.getElementById(albumA).style.transform = 'scale(1.03)';
 };
 
 stop = (player, albumA, albumB) => {
-	document.getElementById(albumB).style.filter = 'grayscale(0)';
-	document.getElementById(albumA).style.transform = 'scale(1)';
+	// document.documentElement.style.backgroundColor = "#FFEBCD";
 	document.getElementById(player).pause();
+	// document.getElementById(albumB).style.filter = 'grayscale(0)';
+	// document.getElementById(albumA).style.transform = 'scale(1)';
 };
 
 choose = (chosen) => {
@@ -96,7 +115,8 @@ choose = (chosen) => {
 	
 	// console.log(app.data.chosen_songs.length);
 	if(app.data.chosen_songs.length % 5 == 0){
-			get_recomended();
+		get_recomended();
+		// document.getElementById("rec_nav").style.display = "flex";//needs to be flex because of bulma
 	}
 	
 	if(app.data.chosen_songs.length > 5){
@@ -170,3 +190,39 @@ app.init = async () => {
 };
 
 app.init();
+
+//non vue js funcs
+
+function hideAll(){
+	const list = ["dome", "rec"];
+	
+	for(let i = 0; i < list.length; i++){
+		document.getElementById(list[i]).setAttribute("hidden", "hidden");
+	}
+}
+
+function toggleElement(element){
+	var t = document.getElementById(element);
+	
+	if(t.getAttribute("hidden")){
+		hideAll();
+		t.removeAttribute("hidden");
+	}
+}
+
+// prevent multiple audio sources from playing at once, works even when items change
+function onlyPlayOneIn(container){
+	container.addEventListener("play", function(event){
+		audio_elements = container.getElementsByTagName("audio")
+		for(i=0; i < audio_elements.length; i++){
+			audio_element = audio_elements[i];
+			if (audio_element !== event.target){
+				audio_element.pause();
+			}
+		}
+	}, true);
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  onlyPlayOneIn(document.body);
+});
